635bd97af7a157ac65d79ecad3454139d14c89e9
ChocolateMagnate <chocolate-magnate@protonmail.com>
2024-06-22 17:04:43 +0300

    handled reading and processing errors for the main Spark.toml file used in the build system.

---
+++ spark/__main__.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import argparse
+import platform
+import sys
+import tomllib
+
+import codes
+from lib import perror
+
+SPARK_MINIMAL_PYTHON_VERSION = "3.10.0"
+
+
+def load_sparkfile() -> dict:
+    try:
+        with open("Spark.toml", 'r') as sparkfile:
+            return tomllib.loads(sparkfile.read())
+    except OSError as e:
+        perror("can't open Spark.toml", e, codes.EXIT_SPARKFILE_UNAVAILABLE)
+
+
+def main(argv: list[str]) -> None:
+    if platform.python_version() < SPARK_MINIMAL_PYTHON_VERSION:
+        sys.stderr.write("spark: You are running Spark in an unsupported version of Python. Spark requires " +
+                         "Python " + SPARK_MINIMAL_PYTHON_VERSION + " and you are running Python " +
+                         platform.python_version() + ".")
+        sys.exit(codes.EXIT_UNSUPPORTED_PYTHON_VERSION)
+
+    parser = argparse.ArgumentParser(prog="spark", description="A declarative build system for C and C++ projects")
+    parser.add_argument("--new", dest="title", type=bool, help="Specifies the name of new Spark project to create.")
+
+    args = parser.parse_args(argv)
+
+    spark_build_declaration: dict = load_sparkfile()
+
+
+if __name__ == "__main__":
+    argv = sys.argv
+    argv.pop(0)  # The first argument in sys.argv is the file the Python Interpreter was called upon that we don't need.
+    main(argv)
---
+++ spark/lib/__init__.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+
+def perror(message: str, exception: OSError, code: int = 0) -> None:
+    """Pythonic analogue of the C perror(3) function. It provides a simple way to print error
+    messages along with human-readable errno variable. It is a common way how errors are printed
+    in Unix-like operating systems and Spark joins a shared convention with a number of other
+    command line utilities that follow this convention.
+    :param message The error message to print to standard error stream.
+    :param exception The OSError exception object that raised the error. perror(3) is commonly
+    only used with operating system related errors, and so far we will continue this convention.
+    :param code (optional) The error code. If the error is fatal, and we cannot continue after it,
+    supply any error code and the function will exit with it."""
+    errno: int = exception.errno
+    errno_message: str = os.strerror(errno)
+    sys.stderr.write("spark: " + message + ": " + errno_message)
+    if code != 0:
+        sys.exit(code)
